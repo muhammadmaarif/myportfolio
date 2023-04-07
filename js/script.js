0069a2183c5c2889ef52a1510ac8b87212967e6c
muhammadmaarif <25muhammad.arif@gmail.com>
2023-04-07 15:41:30 +0700

    menambahkan galeri foto

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("nav-dark");
   }
 });
+
+// portfolio
+const elementclose = document.getElementsByClassName("overlay");
+for (let i = 0; i < elementclose.length; i++) {
+  elementclose[i].addEventListener("click", function () {
+    let url = window.location.href.substr(0, window.location.href.indexOf("#"));
+    window.location = url;
+  });
+}
